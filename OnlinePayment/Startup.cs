f622d700f914f1e3088df24a8a3ed8d2c5fb32dd
osingh <oyeshsin@hotmail.com>
2016-03-13 22:18:46 -0500

    initial

---
+++ OnlinePayment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlinePayment.Startup))]
+namespace OnlinePayment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
